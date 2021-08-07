baf3c7fe030a76cd014f1e3df4485a8691380f13
iCode_Rx <arababs2015@gmail.com>
2021-08-07 13:10:23 +0100

    useRef updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import StateHook from "./components/useState"
 import ReducerHook from "./components/useReducer"
 import EffectHook from "./components/useEffect"
+import RefHook from "./components/useRef"
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
 			<ReducerHook />
 
 			<EffectHook />
+
+			<RefHook />
 		</div>
 	)
 }
--- src/components/useRef.jsx
+++ src/components/useRef.jsx
@@ -1,18 +1,28 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 // useRef is used to manipulate DOM elemt
 
 export default function RefHook() {
           const inputRef = useRef(null)
+          const [input, setInput ] = useState("")
 
           const onClicked = () => {
+                    // wn d btn is click d input box is active
+                    inputRef.current.focus()
+
+                    setInput( inputRef.current.value )
+                    
                     console.log(inputRef.current.value);
+
+                    // remove d input value after clicking d btn
+                    inputRef.current.value = ""
+                    
           }
 
           return (
                     <div>
                               <h1> UseRef: </h1>
 
-                              <h1> Arabog: </h1>
+                              <h1> Arabog: {input }</h1>
 
                               <input 
                                         type="text" 
